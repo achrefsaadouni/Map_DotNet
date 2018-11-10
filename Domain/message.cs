b0c1c56ce1339a4bd3a438b1e834bd2a51b013c1
Karim0210 <36571315+Karim0210@users.noreply.github.com>
2018-11-11 00:33:28 +0100

    firstcommit

--- Domain/message.cs
+++ Domain/message.cs
@@ -4,7 +4,6 @@ namespace Domain
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
 
     [Table("map.message")]
     public partial class message
