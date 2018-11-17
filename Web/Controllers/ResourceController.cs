fa36eba3ad1a3de7aee2f489af95a07ecc170e2d
ineshb220 <ines.hbibi@esprit.tn>
2018-11-17 13:01:47 +0100

    ines 17/11/2018

--- Web/Controllers/ResourceController.cs
+++ Web/Controllers/ResourceController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Domain;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Mvc;
 using Web.Models;
 
@@ -35,19 +38,25 @@ namespace Web.Controllers
             return View("AddResource");
         }
 
-     
+
         [HttpPost]
-        public ActionResult AddResource(ResourceViewModel resource)
-        {
+         public ActionResult AddResource(ResourceViewModel resource , HttpPostedFileBase file)
+         {
            
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("http://127.0.0.1:18080");
-            client.PostAsJsonAsync<ResourceViewModel>("Map-JavaEE-web/MAP/Resources/add",resource);
-            return RedirectToAction("listeResource");
-        }
+             string fileName = Path.GetFileNameWithoutExtension(file.FileName);
+             string extension = Path.GetExtension(file.FileName);
+             fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+             resource.picture = "~/Image/" + fileName;
+             fileName = Path.Combine(Server.MapPath("~/Image/"), fileName);
+             file.SaveAs(fileName);
+             HttpClient client = new HttpClient();
+             client.BaseAddress = new Uri("http://127.0.0.1:18080");
+             client.PostAsJsonAsync<ResourceViewModel>("Map-JavaEE-web/MAP/Resources/add",resource);
+              return RedirectToAction("listeResource");
+            }
 
         // GET: Resource/Deateil
-        public ActionResult DetailResource(int? id)
+        public ActionResult DetailResource(int id)
         {
             ResourceViewModel resource = new ResourceViewModel();
             //string idResource = Convert.ToString(id);
@@ -55,10 +64,9 @@ namespace Web.Controllers
             HttpClient Client = new HttpClient();
             Client.BaseAddress = new Uri("http://127.0.0.1:18080");
             Client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-            HttpResponseMessage response = Client.GetAsync("Map-JavaEE-web/MAP/Resources/resourceById?resourceId=id.ToString()").Result;
+            HttpResponseMessage response = Client.GetAsync("Map-JavaEE-web/MAP/Resources/resourceById?resourceId="+id).Result;
             if (response.IsSuccessStatusCode)
             {
-               
                 resource = response.Content.ReadAsAsync<ResourceViewModel>().Result;
                 
             }
@@ -67,6 +75,69 @@ namespace Web.Controllers
         }
 
 
+
+        // GET: Resource/Archiver
+        public ActionResult Archiver(int? id)
+        {
+            ResourceViewModel resource = new ResourceViewModel();
+            HttpClient Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://127.0.0.1:18080");
+            Client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = Client.GetAsync("Map-JavaEE-web/MAP/Resources/resourceById?resourceId="+id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                resource = response.Content.ReadAsAsync<ResourceViewModel>().Result;
+
+            }
+            else { ViewBag.result = "erreur"; }
+            return View(resource);
+        }
+
+        
+        [HttpPost]
+        public ActionResult Archiver(ResourceViewModel rvm)
+        {
+            HttpClient Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://127.0.0.1:18080");
+            Client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("text/plain"));
+       
+            HttpResponseMessage response = Client.PutAsJsonAsync<ResourceViewModel>("Map-JavaEE-web/MAP/Resources/archive?resourceId="+rvm.id, rvm).Result;
+
+            return RedirectToAction("listeResource");
+        }
+
+
+
+
+        // GET: Resource/UpdateResource
+        public ActionResult UpdateResource(int id)
+        {
+            ResourceViewModel resource = new ResourceViewModel();
+            HttpClient Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://127.0.0.1:18080");
+            Client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = Client.GetAsync("Map-JavaEE-web/MAP/Resources/resourceById?resourceId=" + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                resource = response.Content.ReadAsAsync<ResourceViewModel>().Result;
+
+            }
+            else { ViewBag.result = "erreur"; }
+            return View(resource);
+        }
+
+        // POST: ResourceViewModels/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UpdateResource([Bind(Include = "id,seniority,workProfil,salary,picture,moyenneSkill,jobType,cv,businessSector,availability,firstName,lastName,email,archived")] ResourceViewModel resourceViewModel)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://127.0.0.1:18080");
+            HttpResponseMessage response = client.PutAsJsonAsync<ResourceViewModel>("Map-JavaEE-web/MAP/Resources/update", resourceViewModel).Result;
+            return RedirectToAction("listeResource");
+        }
 
     }
 }
--- Web/Models/ResourceViewModel.cs
+++ Web/Models/ResourceViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Domain.Enumeration;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -12,17 +13,26 @@ namespace Web.Models
 
         public int id { get; set; }
         public SeniorityType seniority { get; set; }
+        [DisplayName("Work profile")]
         public WorkType workProfil { get; set; }
         public float salary { get; set; }
         public string picture { get; set; }
+        [DisplayName("Average skills")]
         public float moyenneSkill { get; set; }
+        [DisplayName("Job Type")]
         public JobType jobType { get; set; }
         public string cv { get; set; }
+        [DisplayName("Business Sector")]
         public string businessSector { get; set; }
         public AvailibilityType availability { get; set; }
+        [DisplayName("First Name")]
         public string firstName { get; set; }
+        [DisplayName("Last Name")]
         public string lastName { get; set; }
         public string email { get; set; }
         public int archived { get; set; }
+        public string password { get; set; }
+
+        //public HttpPostedFileBase ImageFile { get; set; }
     }
 }
