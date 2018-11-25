654584fa6d2ccb5c6c3d90a5a85fbf86baece9ec
Jemmali <mohamedemir.jemmali@esprit.tn>
2018-11-25 22:51:24 +0100

    StatProjectSide

--- Web/Controllers/StatAdminController.cs
+++ Web/Controllers/StatAdminController.cs
@@ -10,6 +10,38 @@ namespace Web.Controllers
     public class StatAdminController : Controller
     {
         // GET: StatAdmin
+        public ActionResult Index()
+        {
+            System.Net.Http.HttpClient Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://127.0.0.1:18080");
+            Client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            //CandidateByresultTest
+            HttpResponseMessage response = Client.GetAsync("Map-JavaEE-web/MAP/StatCandidate/CountCadidateResultByTypeTest").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var result = response.Content.ReadAsStringAsync().Result;
+                var s = Newtonsoft.Json.JsonConvert.DeserializeObject(result);
+                ViewBag.result1 = result.ToString().Replace("&quot;", " ");
+            }
+            else
+            {
+                ViewBag.result1 = "erreur";
+            }
+            //CandidateByState
+            HttpResponseMessage response1 = Client.GetAsync("Map-JavaEE-web/MAP/StatAdmin/CountNbCandidateByState").Result;
+            if (response1.IsSuccessStatusCode)
+            {
+                var result1 = response1.Content.ReadAsStringAsync().Result;
+                var s1 = Newtonsoft.Json.JsonConvert.DeserializeObject(result1);
+                ViewBag.result2 = result1.ToString().Replace("&quot;", " ");
+            }
+            else
+            {
+                ViewBag.result2 = "erreur";
+            }
+
+            return View();
+        }
         public ActionResult GeneralInformation()
         {
             //ClientByCategory
@@ -105,6 +137,21 @@ namespace Web.Controllers
         }
         public ActionResult Rh()
         {
+            System.Net.Http.HttpClient Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://127.0.0.1:18080");
+            Client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            //CandidateByresultTest
+            HttpResponseMessage response = Client.GetAsync("Map-JavaEE-web/MAP/StatAdmin/CountNbRhByAvailability").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var result = response.Content.ReadAsStringAsync().Result;
+                var s = Newtonsoft.Json.JsonConvert.DeserializeObject(result);
+                ViewBag.result1 = result.ToString().Replace("&quot;", " ");
+            }
+            else
+            {
+                ViewBag.result1 = "erreur";
+            }
             return View();
         }
 
@@ -114,6 +161,21 @@ namespace Web.Controllers
         }
         public ActionResult Project()
         {
+            System.Net.Http.HttpClient Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://127.0.0.1:18080");
+            Client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            //CandidateByresultTest
+            HttpResponseMessage response = Client.GetAsync("Map-JavaEE-web/MAP/StatAdmin/BeneficeByProject").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var result = response.Content.ReadAsStringAsync().Result;
+                var s = Newtonsoft.Json.JsonConvert.DeserializeObject(result);
+                ViewBag.result1 = result.ToString().Replace("&quot;", " ");
+            }
+            else
+            {
+                ViewBag.result1 = "erreur";
+            }
             return View();
         }
 
